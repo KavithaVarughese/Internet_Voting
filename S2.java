2d84564f06e9cabba8a535ab25b9661d64c33eff
mattyco <mathewg98@gmail.com>
2020-06-10 07:02:18 +0530

    Fixing comments

--- S2.java
+++ S2.java
@@ -1,6 +1,5 @@
-// Java implementation of Server side 
-// It contains two classes : Server and ClientHandler 
-// Save file as Server.java 
+// Java implementation of Server 2
+// Saved file as Server2.class
 
 import java.io.*; 
 import java.text.*; 
